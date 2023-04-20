b02f549df6ec9686fa260d14640f699404c22eb9
sseni <jiseon1113@naver.com>
2023-04-20 09:42:43 +0900

    feat: CodeUp 1~21 추가

---
+++ src/main/java/com/example/javaproject3/codeup/CodeUp1020.java
@@ -0,0 +1,11 @@
+package com.example.javaproject3.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1020 {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        String[] me = sn.next().split("-");
+        System.out.println(me[0] + me[1]);
+    }
+}
