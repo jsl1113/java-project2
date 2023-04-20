7b07c75944fe659d8712c1432705e00f1154d87a
sseni <jiseon1113@naver.com>
2023-04-20 15:31:23 +0900

    feat: CodeUp 1014 추가

--- src/main/java/com/example/javaproject3/codeup/CodeUp1014.java
+++ src/main/java/com/example/javaproject3/codeup/CodeUp1014.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class CodeUp1014 {
     public static void main(String[] args) {
-        Scanner sn = new Scanner(System.in);
-        char x = sn.next().charAt(0);
-        char y = sn.next().charAt(0);
-        System.out.println(y + " " + x);
+        Scanner sc = new Scanner(System.in);
+        char c1 = sc.next().charAt(0);
+        char c2 = sc.next().charAt(0);
+        System.out.println(c2 + " " + c1);
 
     }
 }
