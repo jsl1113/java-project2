5cc2f28df19c6cdbedd3b4571751357b31224821
sseni <jiseon1113@naver.com>
2023-04-20 09:25:00 +0900

    feat: ScannerEx2 추가

---
+++ src/main/java/com/example/javaproject3/week1/ScannerExTest2.java
@@ -0,0 +1,10 @@
+package com.example.javaproject3.week1;
+
+import java.util.Scanner;
+
+public class ScannerExTest2 {
+    public static void main(String[] args) {
+        ScannerEx2 sn2 = new ScannerEx2();
+        sn2.readTwoNumbersAndPlus();
+    }
+}
