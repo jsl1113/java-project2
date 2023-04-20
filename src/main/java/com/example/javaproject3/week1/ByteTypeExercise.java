ee2f30d93826c69eb51edf30df6ae2de05710d5e
sseni <jiseon1113@naver.com>
2023-04-20 14:35:29 +0900

    feat: ScannerFormatting, ByteTypeExercise.java 추가

---
+++ src/main/java/com/example/javaproject3/week1/ByteTypeExercise.java
@@ -0,0 +1,11 @@
+package com.example.javaproject3.week1;
+
+public class ByteTypeExercise {
+    public static void main(String[] args) {
+        byte bValFrom = -128;
+        byte bValTo = 127;
+        System.out.println(bValFrom);
+        System.out.println(bValTo);
+
+    }
+}
---
+++ src/main/java/com/example/javaproject3/week1/PrintFloatDigit.java
@@ -0,0 +1,9 @@
+package com.example.javaproject3.week1;
+
+public class PrintFloatDigit {
+    public static void main(String[] args) {
+        System.out.printf("%f\n", 3.389);
+        System.out.printf("%.1f\n", 3.389);
+        System.out.printf("%.2f\n", 3.389);
+    }
+}
