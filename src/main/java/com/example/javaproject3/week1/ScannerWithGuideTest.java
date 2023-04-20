cf1c87e1d675b9256147b58c38a8ed58cfc118bb
sseni <jiseon1113@naver.com>
2023-04-20 10:38:35 +0900

    feat: ScannerWithGuide.java 추가

---
+++ src/main/java/com/example/javaproject3/week1/ScannerWithGuideTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject3.week1;
+
+public class ScannerWithGuideTest {
+    public static void main(String[] args) {
+        ScannerWithGuide swg = new ScannerWithGuide();
+        swg.printWithScanner();
+    }
+}
