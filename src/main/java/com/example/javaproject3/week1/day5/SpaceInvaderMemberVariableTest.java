995737e2d25581ada9f6107560b459b4294ad862
sseni <jiseon1113@naver.com>
2023-04-21 11:53:35 +0900

    feat: SpaceInvaderMemberVariableTest.java 추가

---
+++ src/main/java/com/example/javaproject3/week1/day5/SpaceInvaderMemberVariableTest.java
@@ -0,0 +1,10 @@
+package com.example.javaproject3.week1.day5;
+
+public class SpaceInvaderMemberVariableTest {
+    public static void main(String[] args) {
+        SpaceInvadersMemberVariable simv = new SpaceInvadersMemberVariable();
+        simv.moveLeft();
+        simv.moveLeft();
+        simv.moveRight();
+    }
+}
---
+++ src/main/java/com/example/javaproject3/week1/day5/SpaceInvadersMemberVariable.java
@@ -0,0 +1,11 @@
+package com.example.javaproject3.week1.day5;
+
+public class SpaceInvadersMemberVariable {
+    int location;
+    public void moveLeft(){
+        location = location -1;
+    }
+    public void moveRight(){
+        location = location + 1;
+    }
+}
