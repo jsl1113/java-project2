eea2b7d1c81ae893ea7ed00a64eed141e8850da2
sseni <jiseon1113@naver.com>
2023-04-21 13:20:30 +0900

    feat: SpaceInvaderMemberVariable.java 추가

--- src/main/java/com/example/javaproject3/week1/day5/SpaceInvadersMemberVariable.java
+++ src/main/java/com/example/javaproject3/week1/day5/SpaceInvadersMemberVariable.java
@@ -4,8 +4,19 @@ public class SpaceInvadersMemberVariable {
     int location;
     public void moveLeft(){
         location = location -1;
+        System.out.printf("Move Left: %d\n", location);
     }
     public void moveRight(){
         location = location + 1;
+        System.out.printf("Move Right: %d\n", location);
+    }
+
+    public static void main(String[] args) {
+        SpaceInvadersMemberVariable simv = new SpaceInvadersMemberVariable();
+        simv.moveLeft();
+        simv.moveRight();
+        simv.moveRight();
+        simv.moveRight();
+        simv.moveRight();
     }
 }
