caeae7616daae91090e70d8f261e29bcd0d35864
sseni <jiseon1113@naver.com>
2023-04-24 14:39:42 +0900

    feat: BooleanEx 추가

---
+++ src/main/java/com/example/javaproject3/week2/day6/BooleanEx.java
@@ -0,0 +1,16 @@
+package com.example.javaproject3.week2.day6;
+
+public class BooleanEx {
+
+    public static void main(String[] args) {
+        boolean flag1 = false;
+        boolean flag2 = true;
+        boolean isPaymentSuccess = false;  // 밸런스-잔액 : 잔액이 충분한지?
+        boolean isBalanceSufficient = true;
+
+        boolean result1 = 1 == 1;  // 1 과 1 이 같은지의 여부(==)를 result1에 대입(=)해라
+        boolean result2 = 1 == 2;
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+}
