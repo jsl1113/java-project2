addd2d837fc6e85bf88d37244b17be1d6a67ef26
sseni <jiseon1113@naver.com>
2023-04-25 10:54:34 +0900

    feat: CodeUp 1063~1065, StringConcat 추가

---
+++ src/main/java/com/example/javaproject3/week2/day7/StringConcat.java
@@ -0,0 +1,9 @@
+package com.example.javaproject3.week2.day7;
+
+public class StringConcat {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "World";
+        System.out.println(s1 + " " + s2);
+    }
+}
