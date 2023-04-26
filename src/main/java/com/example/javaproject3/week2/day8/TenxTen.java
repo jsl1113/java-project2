e49db41f0c286328d9df631e594f80a2cd8ecef4
sseni <jiseon1113@naver.com>
2023-04-26 10:38:25 +0900

    fix: TexTen 수정

--- src/main/java/com/example/javaproject3/week2/day8/TenxTen.java
+++ src/main/java/com/example/javaproject3/week2/day8/TenxTen.java
@@ -11,15 +11,19 @@ public class TenxTen {
         System.out.println(Arrays.toString(arr[2]));
 
         int[][] arr10x10 = new int[10][10];
-        System.out.println(Arrays.toString(arr10x10[0]));
-        System.out.println(Arrays.toString(arr10x10[1]));
-        System.out.println(Arrays.toString(arr10x10[2]));
-        System.out.println(Arrays.toString(arr10x10[3]));
-        System.out.println(Arrays.toString(arr10x10[4]));
-        System.out.println(Arrays.toString(arr10x10[5]));
-        System.out.println(Arrays.toString(arr10x10[6]));
-        System.out.println(Arrays.toString(arr10x10[7]));
-        System.out.println(Arrays.toString(arr10x10[8]));
-        System.out.println(Arrays.toString(arr10x10[9]));
+//        System.out.println(Arrays.toString(arr10x10[0]));
+//        System.out.println(Arrays.toString(arr10x10[1]));
+//        System.out.println(Arrays.toString(arr10x10[2]));
+//        System.out.println(Arrays.toString(arr10x10[3]));
+//        System.out.println(Arrays.toString(arr10x10[4]));
+//        System.out.println(Arrays.toString(arr10x10[5]));
+//        System.out.println(Arrays.toString(arr10x10[6]));
+//        System.out.println(Arrays.toString(arr10x10[7]));
+//        System.out.println(Arrays.toString(arr10x10[8]));
+//        System.out.println(Arrays.toString(arr10x10[9]));
+
+        for(int i=0; i<10; i++){
+            System.out.println(Arrays.toString(arr10x10[i]));
+        }
     }
 }
