4abafee85d937994b3a09dd520a6d48d3d2d9bdf
sseni <jiseon1113@naver.com>
2023-04-26 13:17:55 +0900

    fix: TwoDimArrFillColumn, TwoDimArrFillRow 수정

--- src/main/java/com/example/javaproject3/week2/day8/TwoDimArrFillColumn.java
+++ src/main/java/com/example/javaproject3/week2/day8/TwoDimArrFillColumn.java
@@ -3,11 +3,16 @@ package com.example.javaproject3.week2.day8;
 import java.util.Arrays;
 
 public class TwoDimArrFillColumn {
-    static int[][] arr;
-    public static void fillColumn(int c){
-        for(int i=0; i<arr.length; i++){
-            arr[i][c] = 1;
-        }
+    public static void fillColumn(int[][] arr, int col){
+//        for(int i=0; i<arr.length; i++){
+//            arr[i][col] = 1;
+//        }
+        arr[0][col] = 1;
+        arr[1][col] = 1;
+        arr[2][col] = 1;
+        arr[3][col] = 1;
+        arr[4][col] = 1;
+        printArray(arr);
     }
     public static void printArray(int[][] arr){   // parameter
         for(int i=0; i<arr.length; i++){
@@ -16,8 +21,7 @@ public class TwoDimArrFillColumn {
         System.out.println("--------------------");
     }
     public static void main(String[] args) {
-        arr = new int[5][5];
-        fillColumn(4);
-        printArray(arr);
+        int[][] arr = new int[5][5];
+        fillColumn(arr, 4);
     }
 }
