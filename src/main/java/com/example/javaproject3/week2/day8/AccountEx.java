27792c1b975ae6fe67d34978fe23e2b1d4b2f5b1
sseni <jiseon1113@naver.com>
2023-04-26 17:34:49 +0900

    feat: AccountEx, Accumulate687, StringAccumulate 추가

---
+++ src/main/java/com/example/javaproject3/week2/day8/AccountEx.java
@@ -0,0 +1,11 @@
+package com.example.javaproject3.week2.day8;
+
+public class AccountEx {
+    public static void main(String[] args) {
+        int myAccount = 1_000_000;
+        myAccount = myAccount - 500_000;
+        System.out.println(myAccount);
+        myAccount = myAccount + 4_000_000;
+        System.out.println(myAccount);
+    }
+}
