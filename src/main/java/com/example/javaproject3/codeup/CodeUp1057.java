c3b8a9e24bfbb550467424a12ec3f8a2f338daeb
sseni <jiseon1113@naver.com>
2023-04-27 14:30:40 +0900

    feat: BitOperator, CodeUp1081, 1082 추가 fix: CodeUp1056, 1057 수정

--- src/main/java/com/example/javaproject3/codeup/CodeUp1057.java
+++ src/main/java/com/example/javaproject3/codeup/CodeUp1057.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class CodeUp1057 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // XNOR
         System.out.println(sc.nextInt() == sc.nextInt() ? 1 : 0);
     }
 }
