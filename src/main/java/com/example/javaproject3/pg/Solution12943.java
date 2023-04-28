57509a930f9fc2450dbf58d1b00037143700cf38
sseni <jiseon1113@naver.com>
2023-04-28 11:16:12 +0900

    feat: IF Ex, BubbleSort 추가 fix: Solution12943 수정

--- src/main/java/com/example/javaproject3/pg/Solution12943.java
+++ src/main/java/com/example/javaproject3/pg/Solution12943.java
@@ -5,20 +5,17 @@ import com.example.javaproject3.psstudy.Solution81302;
 
 public class Solution12943 {
     public int solution(int num) {
-        int answer = 0, testCase = 0;
+        int answer = 0;
 
         while (num != 1) {
-            if (testCase == 500) return -1;
-            if (num % 2 == 0) {
-                num /= 2;
-                answer++;
-            } else if (num % 2 == 1) {
-                num *= 3;
-                num++;
-                answer++;
-            }
+            if (num % 2 == 0)  num /= 2;
+             else if (num % 2 == 1) num = num * 3 + 1;
+            answer++;
             if (num == 1) break;
-            else testCase++;
+            if(answer >= 500) {
+                answer = -1;
+                break;
+            }
         }
         return answer;
     }
