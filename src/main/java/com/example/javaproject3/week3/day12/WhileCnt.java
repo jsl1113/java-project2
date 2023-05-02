ee29e0bd69ca1a5962a3b4785bf745c1f61818cc
sseni <jiseon1113@naver.com>
2023-05-02 14:25:32 +0900

    feat: 수열과 구간 쿼리 2,4, WhileEx

---
+++ src/main/java/com/example/javaproject3/week3/day12/WhileCnt.java
@@ -0,0 +1,10 @@
+package com.example.javaproject3.week3.day12;
+
+public class WhileCnt {
+    public static void main(String[] args) {
+        int cnt = 0;
+        while (cnt < 11) {
+            System.out.printf("%d ", cnt++);
+        }
+    }
+}
