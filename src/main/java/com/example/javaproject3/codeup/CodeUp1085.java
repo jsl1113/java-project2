486b4254a5d5750fbee5d0be94d5c942be4e314a
sseni <jiseon1113@naver.com>
2023-05-02 11:46:32 +0900

    feat: ForLoop Ex fix: CodeUp1085 delete: test

--- src/main/java/com/example/javaproject3/codeup/CodeUp1085.java
+++ src/main/java/com/example/javaproject3/codeup/CodeUp1085.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class CodeUp1085 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int h = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int s = sc.nextInt();
-        System.out.printf("%.1f MB\n", (double) h*b*c*s/8388608);
+        long b = sc.nextLong();
+        long h = sc.nextLong();
+        long c = sc.nextLong();
+        long s = sc.nextLong();
+        long bit = (long) (h * b * c * s);
+        float result  = bit / 1024.0f / 1024.0f / 8.0f;
+        System.out.printf("%.1f MB\n", result);
     }
 }
---
+++ src/main/java/com/example/javaproject3/week3/day12/ForLoopCase2.java
@@ -0,0 +1,9 @@
+package com.example.javaproject3.week3.day12;
+
+public class ForLoopCase2 {
+    public static void main(String[] args) {
+        for(int i=0; 2*i <= 17; i++){
+            System.out.printf("%d ", i);
+        }
+    }
+}
