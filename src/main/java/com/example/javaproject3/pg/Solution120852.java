18b7ae3eb4acb1804568de5b4c00b7e6482d40c1
sseni <jiseon1113@naver.com>
2023-05-04 17:22:14 +0900

    feat: CodeUp4596, Solution120852

--- src/main/java/com/example/javaproject3/pg/Solution120852.java
+++ src/main/java/com/example/javaproject3/pg/Solution120852.java
@@ -7,34 +7,24 @@ public class Solution120852 {
     public int[] solution(int n) {
         int[] answer = {};
         int[] tmp = new int[n/2];
-        int num = 2, idx = 0;
         boolean flag = false;
+        int num = 2, idx = 0;
         while(n != 1){
-            while(isPrime(num) && n%num == 0){
+            while(n%num == 0){
                 tmp[idx] = num;
                 n /= num;
                 flag = true;
             }
+
             num++;
-            if(flag) {
+            if(flag){
                 idx++;
                 flag = false;
             }
         }
         answer = new int[idx];
-        for(int i=0; i<idx;i++) answer[i] = tmp[i];
+        for(int i=0; i<idx; i++) answer[i] = tmp[i];
         return answer;
-    }
-
-    public static boolean isPrime(int n){
-        boolean isPrime = true;
-        for(int i=2; i*i <= n; i++){
-            if(n % i == 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        return isPrime;
     }
 
     public static void main(String[] args) {
